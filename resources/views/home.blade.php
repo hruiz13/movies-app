62213c5b7e5204dfae4ea45498e59e3e84634db0
Hector Ruiz <hruiz.com@gmail.com>
2021-05-15 16:59:29 -0500

    navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,20 @@
 @section('title', 'Home')    
 
 @section('content')
-    <h1 class="md:text-4xl sm:text-2xl" >Welcome to movies page</h1>
-    <h2 class="md:text-2xl sm:lg">My last viewed movies!</h2>
-    <div class="grid md:grid-cols-4 sm:grid-cols-2 gap-4">
-        <div class="bg-blue-300 col-span-2">A</div>
-        <div class="bg-blue-300">B</div>
-        <div class="bg-blue-300">C</div>
-        <div class="bg-blue-300">D</div>
-        <div class="bg-blue-300">E</div>
-        <div class="bg-blue-300">F</div>
-        <div class="bg-blue-300">G</div>
+    <div class="m-6">
 
+        <h1>Welcome to movies page</h1>
+        <h2>My last viewed movies!</h2>
+        <div class="grid md:grid-cols-4 sm:grid-cols-2 gap-4">
+            <div class="bg-blue-300 col-span-2">A</div>
+            <div class="bg-blue-300">B</div>
+            <div class="bg-blue-300">C</div>
+            <div class="bg-blue-300">D</div>
+            <div class="bg-blue-300">E</div>
+            <div class="bg-blue-300">F</div>
+            <div class="bg-blue-300">G</div>
+        </div>
+        
     </div>
+
 @endsection
